0f9db92756733df1131d4b81c7c133db070dfe42
UP1992 <panchaludit150@gmail.com>
2025-10-08 10:12:56 +0530

    Connected mongodb to backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoutes.js"; 
 
 dotenv.config();
 const app = express();
@@ -13,11 +15,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+connectDB();
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
