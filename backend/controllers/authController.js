7ff11e76cf36b4009fb9580e957a42da873de28f
UP1992 <panchaludit150@gmail.com>
2025-10-26 11:17:02 +0530

    Created login, Register API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,8 @@
-import User from '../models/userModel.js';
-import jwt from 'jsonwebtoken';
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // generate JWT
-export const generateToken = (id) => {
+const generateToken = (id) => {
   return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: '1h'});
 
 };
@@ -39,10 +39,38 @@ exports.registerUser = async (req, res) => {
 
 //Login user
 exports.loginUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { email, password } = req.body;
+  if(!email || !password) {
+    return  res.status(400).json({message: 'All fields are required'});
+  }
+  try {
+
+    const user = await User.findOne({ email });
+    if(!user || !(await user.matchPassword(password))) {
+      return res.status(400).json({message: 'Invalid credentials'});
+    }
+
+    res.status(200).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({message: 'Error logging in', error: error.message});
+  }
 };
 
 //get user info
 exports.getUserInfo = async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if(!user) {
+      return res.status(404).json({message: 'User not found'});
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({message: 'Error fetching user info', error: error.message});
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js"; 
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
@@ -17,7 +18,7 @@ app.use(express.json());
 
 connectDB();
 
-// app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
